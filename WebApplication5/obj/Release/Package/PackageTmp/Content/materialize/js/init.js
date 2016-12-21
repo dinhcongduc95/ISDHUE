87fb0c0d53e92d05c9801cd7dd13461dcfabcc98
dinhcongduc95 <dinhcongduc95@gmail.com>
2016-12-21 16:58:26 +0700

    Revert "up"
    This reverts commit 62f7bdd0e3133080218e71c9468a32626d67744b.

--- WebApplication5/obj/Release/Package/PackageTmp/Content/materialize/js/init.js
+++
@@ -1,9 +0,0 @@
-(function($){
-  $(function(){
-
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
-    $('select').material_select();
-    Materialize.updateTextFields();
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
